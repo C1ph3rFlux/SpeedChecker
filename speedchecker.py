f98aa10eb6cd7d65d38618a3952b389ec7bb6115
Jeremiah Roe <37093713+C1ph3rFlux@users.noreply.github.com>
2021-01-05 22:59:08 -0500

    Update speedchecker.py
    Added author and copyright information.

--- speedchecker.py
+++ speedchecker.py
@@ -1,3 +1,13 @@
+#!/usr/bin/env python
+
+__author__ = "Jeremiah Roe (C1ph3rFlux)"
+__copyright__ = "Copyright 2021, Jeremiah Roe"
+__credits__ = ["Jeremiah Roe, Maybe You?"]
+__license__ = "MIT"
+__version__ = "N/A"
+__maintainer__ = "Jeremiah Roe (C1ph3rFlux)"
+__email__ = "c1ph3rflux@gmail.com"
+
 from datetime import datetime
 import os
 
@@ -10,8 +20,8 @@ now = datetime.now()
 dt_string = now.strftime("%d/%m/%Y %H:%M:%S")
 print("date and time =", dt_string)
 
-# run network speedtest
-os.system("/usr/local/Cellar/speedtest-cli/2.1.2/bin/speedtest-cli")
+# run network speedtest (Need to edit for your location of "speedtest-cli")
+os.system("/Add/Location/Of/Your/Installation/Of/speedtest-cli")
 
 # insert line break
 print('\n'*1)
